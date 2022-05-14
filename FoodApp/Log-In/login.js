69155df6be4964426bf34484e34af208a8a602af
juanjof1 <salazargiljuanjose@gmail.com>
2022-05-14 14:25:58 -0600

    foodapp

--- FoodApp/Log-In/login.js
+++ FoodApp/Log-In/login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useAuth} from "@auth0/auth0-react";
 
 export const LoginButton = () => {
-    const {loginWitReactdirect} = useAuth0();
+    const {loginWitReactdirect} = useAuth();
 
     return <button onClick={() => loginWitReactdirect}>Login</button>;
 };
